d9c0d3a75d4a95d146d9f29b62ad836dc1d26ca2
njoerdr <clement@eduroam-052-102.scc.uni-weimar.de>
2014-01-24 12:33:04 +0100

    initial upload

---
+++ app.js
@@ -0,0 +1,41 @@
+
+/* The presenter */
+
+(function() { 'use strict';
+  /*
+    A Model instance exposed to global space so you can
+    use the Todo APi from the console. For example:
+
+    todo.add("My task");
+  */
+  /*window.lax = new Lax();*/
+
+  // HTML for a single todo item
+  var template = $("[type='html/tumb']").html();
+  var resultview = $("#resultview");
+
+  for(var i = 0; i != store.length; i++) {
+    var item = {id: i+1, name: store[i]};
+    var el = $($.render(template, item)).appendTo(resultview);
+  }
+
+  $("figure>button").fadeOut();
+  $("figure>figcaption").fadeOut();
+
+  /* Listen to user events */
+
+  $("img").click(function() {
+    console.log(this.id);
+  })
+
+  $("figure").mouseenter(function() {
+    $("button", this).fadeIn();
+    $("figcaption", this).fadeIn();
+  })
+
+  $("figure").mouseleave(function() {
+    $("button", this).fadeOut();
+    $("figcaption", this).fadeOut();
+  })
+
+})()
