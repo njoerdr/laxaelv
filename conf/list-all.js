1ca548f09f197e31597146d6c1dc13440a5c56b6
Marcus Kossatz <marcus.kossatz@uni-weimar.de>
2014-02-16 23:20:58 +0100

    added 57 images

--- conf/list-all.js
+++ conf/list-all.js
@@ -1,50 +1,426 @@
 var store = [
-{name:"00001-poll.jpg",tags:["photo", "house", "estonia", "baltic sea"]},
-{name:"00002-poll.jpg",tags:["photo", "house", "estonia", "baltic sea"]},
-{name:"00003-poll.jpg",tags:["photo", "house", "estonia", "baltic sea"]},
-{name:"00004-poll.jpg",tags:["photo", "house", "estonia", "baltic sea"]},
-{name:"00005-poll.jpg",tags:["photo", "house", "estonia", "baltic sea"]},
-{name:"00006-poll.jpg",tags:["photo", "house", "estonia", "baltic sea"]},
-{name:"00007-poll.jpg",tags:["photo", "house", "estonia", "baltic sea"]},
-{name:"00008-poll.jpg",tags:["photo", "house", "estonia", "baltic sea"]},
-{name:"00009-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00010-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00011-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00012-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00013-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00014-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00015-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00016-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00017-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00018-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00019-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00020-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00021-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00022-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00023-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00024-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00025-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00026-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00027-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00028-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00029-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00030-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00031-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00032-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00033-weimaraner.jpg",tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]},
-{name:"00034-hochwasserhund.jpg",tags:["photo", "dog", "animal"]},
-{name:"00035-segelboot.jpg",tags:["photo", "boat", "sail", "water"]},
-{name:"00036-klammer.jpg",tags:["photo", "art", "cloth pin"]},
-{name:"00037-airsexchampionship.jpg",tags:["photo", "air", "sex", "championship"]},
-{name:"00038-airsexchampionship.jpg",tags:["photo", "air", "sex", "championship"]},
-{name:"00039-boese.jpg",tags:["photo", "weimar", "art", "candle", "anna amalia bibliothek", "commercial"]},
-{name:"00040-affe.jpg",tags:["photo", "monkey", "ape", "selfie"]},
-{name:"00041-gaddafi.jpg",tags:["photo", "politics"]},
-{name:"00042-gnuone.jpg",tags:["photo", "politics", "grumpy cat", "grumpy", "cat"]},
-{name:"00043-lake.jpg",tags:["photo", "lake", "under water", "water"]},
-{name:"00044-konigsberg.jpg",tags:["photo", "house", "architecture", "anthropomorphic"]},
-{name:"00045-poolhouse.jpg",tags:["photo", "house", "architecture", "pool", "luxury", "water"]},
-{name:"00046-poolhouse.jpg",tags:["photo", "house", "architecture", "pool", "luxury", "water"]},
-{name:"00047-venedig.jpg",tags:["photo", "nude", "venice", "black and white"]},
-{name:"00048-cats.jpg",tags:["photo", "cat", "heart", "pink", "animal"]},
-{name:"00049-dali.jpg", tags:["photo", "black and white"]}];
+  {
+    name:"00001-poll.jpg",
+    tags:["photo", "house", "estonia", "baltic sea"]
+  },
+  {
+    name:"00002-poll.jpg",
+    tags:["photo", "house", "estonia", "baltic sea"]
+  },
+  {
+    name:"00003-poll.jpg",
+    tags:["photo", "house", "estonia", "baltic sea"]
+  },
+  {
+    name:"00004-poll.jpg",
+    tags:["photo", "house", "estonia", "baltic sea"]
+  },
+  {
+    name:"00005-poll.jpg",
+    tags:["photo", "house", "estonia", "baltic sea"]
+  },
+  {
+    name:"00006-poll.jpg",
+    tags:["photo", "house", "estonia", "baltic sea"]
+  },
+  {
+    name:"00007-poll.jpg",
+    tags:["photo", "house", "estonia", "baltic sea"]
+  },
+  {
+    name:"00008-poll.jpg",
+    tags:["photo", "house", "estonia", "baltic sea"]
+  },
+  {
+    name:"00009-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00010-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00011-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00012-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00013-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00014-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00015-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00016-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00017-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00018-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00019-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00020-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00021-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00022-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00023-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00024-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00025-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00026-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00027-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00028-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00029-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00030-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00031-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00032-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00033-weimaraner.jpg",
+    tags:["photo", "animal", "weimaraner", "dog", "nature", "art"]
+  },
+  {
+    name:"00034-hochwasserhund.jpg",
+    tags:["photo", "dog", "animal"]
+  },
+  {
+    name:"00035-segelboot.jpg",
+    tags:["photo", "boat", "sail", "water"]
+  },
+  {
+    name:"00036-klammer.jpg",
+    tags:["photo", "art", "cloth pin"]
+  },
+  {
+    name:"00037-airsexchampionship.jpg",
+    tags:["photo", "air", "sex", "championship"]
+  },
+  {
+    name:"00038-airsexchampionship.jpg",
+    tags:["photo", "air", "sex", "championship"]
+  },
+  {
+    name:"00039-boese.jpg",
+    tags:["photo", "weimar", "art", "candle", "anna amalia bibliothek", "commercial"]
+  },
+  {
+    name:"00040-affe.jpg",
+    tags:["photo", "monkey", "ape", "selfie"]
+  },
+  {
+    name:"00041-gaddafi.jpg",
+    tags:["photo", "politics"]
+  },
+  {
+    name:"00042-gnuone.jpg",
+    tags:["photo", "politics", "grumpy cat", "grumpy", "cat"]
+  },
+  {
+    name:"00043-lake.jpg",
+    tags:["photo", "lake", "under water", "water"]
+  },
+  {
+    name:"00044-konigsberg.jpg",
+    tags:["photo", "house", "architecture", "anthropomorphic"]
+  },
+  {
+    name:"00045-poolhouse.jpg",
+    tags:["photo", "house", "architecture", "pool", "luxury", "water"]
+  },
+  {
+    name:"00046-poolhouse.jpg",
+    tags:["photo", "house", "architecture", "pool", "luxury", "water"]
+  },
+  {
+    name:"00047-venedig.jpg",
+    tags:["photo", "nude", "venice", "black and white"]
+  },
+  {
+    name:"00048-cats.jpg",
+    tags:["photo", "cat", "heart", "pink", "animal"]
+  },
+  {
+    name:"00049-dali.jpg",
+    tags:["photo", "black and white"]
+  },
+  {
+    name:"00050-Katainen.jpg",
+    tags:["katainen", "finland", "funny", "beer", "finnish"]
+  },
+  {
+    name:"00051-Katainen.jpg",
+    tags:["katainen", "finland", "conference"]
+  },
+  {
+    name:"00052-Katainen.jpg",
+    tags:["katainen", "finland", "speech", "berlin"]
+  },
+  {
+    name:"00053-Sami.jpg",
+    tags:["black and white", "sami", "finland", "culture", "woman"]
+  },
+  {
+    name:"00054-Sami.jpg",
+    tags:["sami", "finland", "culture", "winter"]
+  },
+  {
+    name:"00055-Sami.jpg",
+    tags:["sami", "santa claus", "norway", "winter"]
+  },
+  {
+    name:"00056-Sami.jpg",
+    tags:["sami", "finland", "reindeer", "winter", "forest"]
+  },
+  {
+    name:"00057-Sami.jpg",
+    tags:["sami", "finland", "culture", "glasses", "funny"]
+  },
+  {
+    name:"00058-Sami.jpg",
+    tags:["sami", "finland", "culture", "boy", "bubble"]
+  },
+  {
+    name:"00059-Sami.jpg",
+    tags:["sami", "group", "culture", "winter", "finland"]
+  },
+  {
+    name:"00060-Sami.jpg",
+    tags:["sami", "house", "black and white", "finland", "forest"]
+  },
+  {
+    name:"00061-Sami.jpg",
+    tags:["sami", "group", "culture", "black and white"]
+  },
+  {
+    name:"00062-Sami.jpg",
+    tags:["sami", "black and white", "culture", "finland", "family"]
+  },
+  {
+    name:"00063-Sami.jpg",
+    tags:["sami", "woman", "culture", "black and white", "finland"]
+  },
+  {
+    name:"00064-Hockey.jpg",
+    tags:["finland", "hockey", "winter", "sports", "flag"]
+  },
+  {
+    name:"00065-Merkel.jpg",
+    tags:["merkel", "death", "speech", "funny"]
+  },
+  {
+    name:"00066-Merkel.jpg",
+    tags:["merkel", "nature", "winter", "stones"]
+  },
+  {
+    name:"00067-Merkel.jpg",
+    tags:["merkel", "germany", "speech"]
+  },
+  {
+    name:"00068-Merkel.jpg",
+    tags:["merkel", "winter", "snow", "nature"]
+  },
+  {
+    name:"00069-Merkel.jpg",
+    tags:["merkel", "winter", "black and white", "sports", "cross-country skiing"]
+  },
+  {
+    name:"00070-Merkel.jpg",
+    tags:["merkel", "winter", "black and white", "sports", "cross-country skiing"]
+  },
+  {
+    name:"00071-Sunset.jpg",
+    tags:["sunset", "baltic sea", "nature"]
+  },
+  {
+    name:"00072-Sunset.jpg",
+    tags:["sunset", "black and white", "finland", "snow", "winter"]
+  },
+  {
+    name:"00073-Sunset.jpg",
+    tags:["sunset", "clouds"]
+  },
+  {
+    name:"00074-Sunset.jpg",
+    tags:["sunset", "lake", "summer"]
+  },
+  {
+    name:"00075-Sunset.jpg",
+    tags:["sunset", "lake", "summer"]
+  },
+  {
+    name:"00076-Sunset.jpg",
+    tags:["sunset", "winter", "nature"]
+  },
+  {
+    name:"00077-Sunset.jpg",
+    tags:["sunset", "nature", "summer"]
+  },
+  {
+    name:"00078-Sunset.jpg",
+    tags:["sunset", "winter", "snowboard", "sports"]
+  },
+  {
+    name:"00079-Sunset.jpg",
+    tags:["sunset", "lake", "nature"]
+  },
+  {
+    name:"00080-Sunset.jpg",
+    tags:["sunset", "baltic sea", "finland", "summer"]
+  },
+  {
+    name:"00081-Finland.jpg",
+    tags:["finland", "winter", "forest", "nature"]
+  },
+  {
+    name:"00082-Finland.jpg",
+    tags:["finland", "baltic sea", "archipelago", "island"]
+  },
+  {
+    name:"00083-Norway.jpg",
+    tags:["norway", "mountain", "lake", "nature"]
+  },
+  {
+    name:"00084-Dali.jpg",
+    tags:["dali", "magnifier", "arts"]
+  },
+  {
+    name:"00085-Dali.jpg",
+    tags:["dali", "arts", "telephone", "black and white", "desk"]
+  },
+  {
+    name:"00086-Dog.jpg",
+    tags:["dog", "animal", "grey"]
+  },
+  {
+    name:"00087-Family.jpg",
+    tags:["family", "nature", "forest"]
+  },
+  {
+    name:"00088-MobilePhoneThrowing.jpg",
+    tags:["finland", "summer", "mobile phone", "sports", "funny"]
+  },
+  {
+    name:"00089-Norway.jpg",
+    tags:["norway", "coast", "red", "house", "ocean"]
+  },
+  {
+    name:"00090-Norway.jpg",
+    tags:["norway", "village", "black and white", "ocean", "coast"]
+  },
+  {
+    name:"00091-Finland.jpg",
+    tags:["finland", "summer", "nature", "lake"]
+  },
+  {
+    name:"00092-Mediterrana.jpg",
+    tags:["spain", "mediterranean", "village", "coast"]
+  },
+  {
+    name:"00093-Dali.jpg",
+    tags:["dali", "jesus", "christ", "cross", "arts"]
+  },
+  {
+    name:"00094-Dali.jpg",
+    tags:["dali", "arts", "surreal"]
+  },
+  {
+    name:"00095-NorthernLight.jpg",
+    tags:["finland", "winter", "northern lights", "aurora borealis", "forest"]
+  },
+  {
+    name:"00096-Mudball.jpg",
+    tags:["finland", "funny", "summer", "sports", "mudball"]
+  },
+  {
+    name:"00097-Animals.jpg",
+    tags:["finland", "winter", "forest", "dog sledge", "sami"]
+  },
+  {
+    name:"00098-Animals.jpg",
+    tags:["animal", "farm", "goat"]
+  },
+  {
+    name:"00099-House.jpg",
+    tags:["house", "wild", "forest"]
+  },
+  {
+    name:"00100-Dino.jpg",
+    tags:["dinosaur", "group", "museum", "ancient", "exhibition"]
+  },
+  {
+    name:"00101-Pony.jpg",
+    tags:["pony", "horse", "animal", "winter"]
+  },
+  {
+    name:"00102-House.jpg",
+    tags:["house", "black and white", "forest"]
+  },
+  {
+    name:"00103-Stone.jpg",
+    tags:["nature", "stone", "gate"]
+  },
+  {
+    name:"00104-Dogs.jpg",
+    tags:["animal", "dogs", "museum", "exhibition"]
+  },
+  {
+    name:"00105-House.jpg",
+    tags:["house", "nature", "surreal", "lonely"]
+  },
+  {
+    name:"00106-House.jpg",
+    tags:["house", "lonely", "field", "nature"]
+  }
+];
--- conf/list-types.js
+++ conf/list-types.js
@@ -1,6 +1,18 @@
 var types = [
-{type:"when",tags:[]},
-{type:"where",tags:["estonia", "baltic sea", "nature", "weimar"]},
-{type:"what",tags:["photo", "house", "animal", "boat"]},
-{type:"who",tags:[]}
+  {
+    type:"when",
+    tags:["summer", "sunset", "winter"]
+  },
+  {
+    type:"where",
+    tags:["estonia", "finland", "baltic sea", "nature", "weimar", "vesuv"]
+  },
+  {
+    type:"what",
+    tags:["photo", "black and white", "house", "animal", "boat"]
+  },
+  {
+    type:"who",
+    tags:["dali", "merkel", "katainen", "sami"]
+  }
 ];
