057b6fc816774b34e6bad0027c5f32740cab1d37
Maximilian Michel <le.maxm@googlemail.com>
2014-02-20 21:59:28 +0100

    tags only draggable in tagcloud

--- scripts/app.js
+++ scripts/app.js
@@ -119,7 +119,7 @@
       else lax.removeTagFromQuery(tagtext);
     });
 
-    $("tagcloud .tag").draggable({ stack: ".tag" });
+    $("#tagcloud .tag").draggable({ stack: ".tag" });
 
     $(".tab").droppable({
       hoverClass: "drophover",
