ce4c73b8f93ddda73bf824e5eba9076f0bb2b309
njoerdr <cwelsch@kth.se>
2014-03-05 14:46:42 +0100

    further upload impl

--- js/upload.js
+++ js/upload.js
@@ -2,9 +2,31 @@
 function Upload() {
 	var droplet = $("[type='html/upload']").html();
 
-	this.render = function() {
+	droplet.ondragover = function() {
+        this.className = 'hover';
+   		return false;
+    };
+    droplet.ondragleave = function () { 
+    	this.className = ''; 
+        return false; 
+    };
+    droplet.ondrop = function(event) {
+    	var form = new FormData();
+        this.className = ''; 
+    	event.preventDefault();
+    	var imgs = event.dataTransfer.files;
+    	for(var i = 0; i < imgs.length; i++) {
+    		if(imgs[i].type === 'image/jpeg') {
+    			form.append('file', imgs[i]);
+    		} else {
+    			alert('Only images of type JPEG are supported.');
+    		}
+    	}
+    };
 
+	this.render = function(parent) {
+		$($.render(droplet)).appendTo(parent);
 	};
 
-	
+
 }
